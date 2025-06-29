008ca3cbe7be613e3f834a8ed811455425c4ad63
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-29 22:44:00 +0100

    database

---
+++ alx_book_store.sql
@@ -0,0 +1,6 @@
+-- stores information about Books available in the store 
+CREATE TABLE Books (book_id PRIMARY KEY, title VARCHAR(130), author_id FOREIGN KEY, price DOUBLE, publication_date DATE);
+CREATE TABLE Authors (author_id PRIMARY KEY, author_name VARCHAR(215));
+CREATE TABLE (customer_id PRIMARY KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
+CREATE TABLE Orders (order_id PRIMARY KEY, customer_id FOREIGN KEY, order_date DATE);
+CREATE TABLE Order_Details (orederdetailid PRIMARY KEY, order_id FOREIGN KEY, book_id FOREIGN KEY, quantity DOUBLE);
