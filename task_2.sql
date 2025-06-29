55a049eb58a32a2488d45a8612400ef11d7e6729
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-30 00:52:17 +0100

    database

--- task_2.sql
+++ task_2.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
-CREATE TABLE books(book_id PRIMARY KEY, title VARCHAR(130), author_id FOREIGN KEY, price DOUBLE, publication_date DATE);
+CREATE TABLE Books(book_id PRIMARY KEY, title VARCHAR(130), author_id FOREIGN KEY, price DOUBLE, publication_date DATE);
 CREATE TABLE Authors(author_id PRIMARY KEY, author_name VARCHAR(215));
 CREATE TABLE Customers(customer_id PRIMARY KEY INT, order_id FOREIGN KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
 CREATE TABLE Orders( order_id PRIMARY KEY, customer_id FOREIGN KEY, order_date DATE);
