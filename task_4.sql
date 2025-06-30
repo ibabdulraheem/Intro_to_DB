566267c1d77034cf15a48e08118fd7ac07799b71
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-30 01:16:30 +0100

    database

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,15 @@
-USE alx_book_store;
-SHOW TABLES;
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE() -- Use the currently selected database (passed as argument)
+    AND TABLE_NAME = 'Books'
+ORDER BY
+    ORDINAL_POSITION;
+
