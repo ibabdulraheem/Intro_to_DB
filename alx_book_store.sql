685b5b40370c0f3ab7d913c4612724800a5d5cf3
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-02 14:25:09 +0100

    database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Books (book_id PRIMARY KEY, title VARCHAR(130), author_id FOREIGN KEY, price DOUBLE, publication_date DATE);
 CREATE TABLE Authors (author_id PRIMARY KEY, author_name VARCHAR(215));
-CREATE TABLE Customers (customer_id INT PRIMARY KEY, order_id FOREIGN KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
-CREATE TABLE Orders (order_id INT PRIMARY KEY, customer_id FOREIGN KEY, order_date DATE);
+CREATE TABLE Customers (customer _id INT PRIMARY KEY, order_id FOREIGN KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
+CREATE TABLE Orders (order_id INT PRIMARY KEY, customer _id FOREIGN KEY, order_date DATE);
 CREATE TABLE Order_Details (orderdetailid PRIMARY KEY, order_id FOREIGN KEY, book_id FOREIGN KEY, quantity DOUBLE);
