888fd85c9a24c861b66623db6c2c84793b62b200
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-02 15:27:46 +0100

    database

--- task_2.sql
+++ task_2.sql
@@ -1,7 +1,7 @@
-USE alx_book_store;
-CREATE TABLE Books(book_id PRIMARY KEY, title VARCHAR(130), author_id FOREIGN KEY, price DOUBLE, publication_date DATE);
-CREATE TABLE Authors(author_id PRIMARY KEY, author_name VARCHAR(215));
-CREATE TABLE Customers(customer_id PRIMARY KEY INT, order_id FOREIGN KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
-CREATE TABLE Orders( order_id PRIMARY KEY, customer_id FOREIGN KEY, order_date DATE);
-CREATE TABLE Order_Details(orderdetailid PRIMARY KEY, order_id FOREIGN KEY, book_id FOREIGN KEY, quantity DOUBLE);
 
+USE alx_book_store;
+CREATE TABLE Books (book_id PRIMARY KEY, title VARCHAR(130), author_id FOREIGN KEY, price DOUBLE, publication_date DATE);
+CREATE TABLE Authors (author_id PRIMARY KEY, author_name VARCHAR(215));
+CREATE TABLE Customers (customer_id INT PRIMARY KEY, order_id FOREIGN KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
+CREATE TABLE Orders (order_id INT PRIMARY KEY,FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), order_date DATE );
+CREATE TABLE Order_Details (orderdetailid PRIMARY KEY, order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id), book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id), quantity DOUBLE);
